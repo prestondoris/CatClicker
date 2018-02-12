b9d135ded30f5544485733c111ecfa014c3d2223
Preston Doris <preston@prestondoris.com>
2018-02-11 19:58:07 -0800

    Update Octpus object

--- script.js
+++ script.js
@@ -1,3 +1,112 @@
+$(function(){
+
+    var model = {
+        currentCat = null;
+        cats = [
+            {
+                name: "Flynn",
+                img: "cat1.jpg",
+                count: 0;
+            },
+            {
+                name: "Jack and Jill",
+                img: "cat2.jpg",
+                count: 0;
+            },
+            {
+                name: "Tony Stark",
+                img: "cat3.jpg",
+                count: 0;
+            },
+            {
+                name: "Mittens",
+                img: "cat4.jpg",
+                count: 0;
+            },
+            {
+                name: "George",
+                img: "cat5.jpg",
+                count: 0;
+            }
+        ]
+    }
+
+    var octopus = {
+        init: function() {
+            model.currentCat = model.cats[0];
+            viewList.init();
+            viewDisplay.init();
+        },
+
+        // 1. Get the current cat that is selected
+        currentCat: function() {
+            return model.currentCat;
+        },
+
+        // 2. Set the selected cat to the Current Cat
+        setNewCurrentCat: function(cat) {
+            model.currentCat = cat;
+        },
+
+        // 3. Be able to send all the cats to the list view
+        // 4. Increment the count for the cat that is clicked.
+        incrementCount: function() {
+            var cat = model.currentCat;
+            
+        },
+
+        postCount: function() {
+
+        },
+
+        getData: function() {
+            return model;
+        }
+    }
+
+    var viewList = {
+        init: function() {
+            var list = $('.catList');
+
+        }
+
+        // This function will display the list of cats for the user.
+        render: function() {
+
+        }
+    }
+
+    var viewDisplay = {
+        init: function() {
+            // Get the DOM elements inside the displayArea and store them for
+            // use later.
+            this.catNameElm = $('.catName');
+            this.catCountElm = $('.catCount');
+            this.catImgElm = $('.catImg');
+
+            // allow a user to click on the current cat picture in increment count
+            this.catImgElm.addEventListener('click', (function(){})(
+
+            ));
+
+            this.render();
+        }
+
+        render: function() {
+            // Get the current Cat from the octopus
+            var currentCat = octopus.currentCat();
+
+            //put the current cat into the view.
+            this.catNameElm.textContent = current.name;
+            this.catCountElm.textContent = current.count;
+            this.catImgElm.innerHTML = '<img src="'+ current.img +'">'
+        }
+    }
+
+    octupus.init();
+});
+
+/*
 var ul = document.getElementById('catList');
 var displayArea = document.getElementById('displayArea');
 var catList = ["cat1.jpg", "cat2.jpg", "cat3.jpg", "cat4.jpg", "cat5.jpg"];
@@ -6,9 +115,9 @@ var catCount = [0,0,0,0,0];
 
 
 for(var i=0; i < catList.length; i++) {
-    var newLi = document.createElement('li');
-    var newImg = document.createElement('img');
-    var span = document.createElement('span');
+
+
+
     span.textContent = catNames[i]
     newImg.setAttribute('src', catList[i]);
     newImg.setAttribute('class', 'catImg');
@@ -44,7 +153,7 @@ function createDisplayItem(index) {
 }
 
 
-/*
+
 var count = document.querySelectorAll('.count');
 var catImg = document.querySelectorAll('.catImg');
 var catName = document.querySelectorAll('.catName');
